6b7279199838b72dffe9a9c8a18e1499d1cd20aa
ngai to <ngai_to_lo@hotmail.com>
2017-08-22 12:01:33 -0700

    d

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/deploy'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -16,3 +16,11 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+// app.listen(8080,function(req,res){
