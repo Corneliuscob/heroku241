d92b40505be7523e335eb8ea1c995c4986a3ad1a
ngai to <ngai_to_lo@hotmail.com>
2017-08-22 11:43:32 -0700

    added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-pp.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 console.log("server is running on port 5000");
