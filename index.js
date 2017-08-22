bf41bac48b4453ae7098defa222fb034e83b871d
ngai to <ngai_to_lo@hotmail.com>
2017-08-22 11:25:03 -0700

    tourble

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
-var server = app.listen(3000);
-console.log("server is running on port 3000");
+var server = app.listen(5000);
+console.log("server is running on port 5000");
 app.use(express.static( 'deploy',{index:false,extensions:['html']}));
+app.set('port', (process.env.PORT || 5000));
+app.set(express.static(__dirname + '/public'));
